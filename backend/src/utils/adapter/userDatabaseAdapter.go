5f0460f3b3d3af6d37fa3270217ab8a75e01aaa4
Misha Diagilev <mishadyagilev@gmail.com>
2023-05-23 23:37:25 +0300

    fix first page, add recursion to lvls of second page

--- backend/src/utils/adapter/userDatabaseAdapter.go
+++ backend/src/utils/adapter/userDatabaseAdapter.go
@@ -40,6 +40,54 @@ func CreateUserDatabaseAdapter(database *database.DBConnect) *UserDatabase {
 	return adapter
 }
 
+func (adapter *UserDatabase) getAllToursRecursive(ctx context.Context, preferences []string, currentIndex int, currentResult *Result, results *[]*Result) error {
+	if currentIndex == len(preferences) {
+		*results = append(*results, currentResult)
+		return nil
+	}
+
+	// Получаем текущее предпочтение
+	preference := preferences[currentIndex]
+	query := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preference, preference)
+
+	rows, err := adapter.database.Connection.Query(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		result := *currentResult
+
+		switch currentIndex {
+		case 0:
+			if err := rows.Scan(&result.First, &result.FirstCountry); err != nil {
+				return err
+			}
+		case 1:
+			if err := rows.Scan(&result.Second, &result.SecondCountry); err != nil {
+				return err
+			}
+		case 2:
+			if err := rows.Scan(&result.Third, &result.ThirdCountry); err != nil {
+				return err
+			}
+		case 3:
+			if err := rows.Scan(&result.Fourth, &result.FourthCountry); err != nil {
+				return err
+			}
+		}
+
+		// Рекурсивно вызываем следующий уровень предпочтений
+		err := adapter.getAllToursRecursive(ctx, preferences, currentIndex+1, &result, results)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (adapter *UserDatabase) GetAllTours(UserFromCountry, UserPreferences, UserFilters string) (results []*Result) {
 	ctx := context.Background()
 	results = make([]*Result, 0)
@@ -68,141 +116,148 @@ func (adapter *UserDatabase) GetAllTours(UserFromCountry, UserPreferences, UserF
 		return
 	} else {
 		preferences := strings.Split(UserPreferences, ",")
-		query := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[0], preferences[0])
-		rows1, err := adapter.database.Connection.Query(ctx, query)
+		currentResult := &Result{}
+		err := adapter.getAllToursRecursive(ctx, preferences, 0, currentResult, &results)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		defer rows1.Close()
-
-		for rows1.Next() {
-			result := &Result{}
-			if err := rows1.Scan(&result.First, &result.FirstCountry); err != nil {
-				log.Fatal(err)
-			}
-
-			querySecond := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[1], preferences[1])
-			rows2, err := adapter.database.Connection.Query(ctx, querySecond)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			defer rows2.Close()
-
-			for rows2.Next() {
-				tempResultSecond := Result{
-					First:        result.First,
-					FirstCountry: result.FirstCountry,
-				}
-				if err := rows2.Scan(&tempResultSecond.Second, &tempResultSecond.SecondCountry); err != nil {
-					log.Fatal(err)
-				}
-
-				if len(preferences) == 3 {
-					queryThird := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[2], preferences[2])
-					rows3, err := adapter.database.Connection.Query(ctx, queryThird)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					defer rows3.Close()
-
-					for rows3.Next() {
-						tempResultThird := Result{
-							First:         tempResultSecond.First,
-							FirstCountry:  tempResultSecond.FirstCountry,
-							Second:        tempResultSecond.Second,
-							SecondCountry: tempResultSecond.SecondCountry,
-						}
-						if err := rows3.Scan(&tempResultThird.Third, &tempResultThird.ThirdCountry); err != nil {
-							log.Fatal(err)
-						}
-
-						if len(preferences) == 4 {
-							queryFourth := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[3], preferences[3])
-							rows4, err := adapter.database.Connection.Query(ctx, queryFourth)
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							defer rows4.Close()
-
-							for rows4.Next() {
-								tempResultFourth := Result{
-									First:         tempResultSecond.First,
-									FirstCountry:  tempResultSecond.FirstCountry,
-									Second:        tempResultSecond.Second,
-									SecondCountry: tempResultSecond.SecondCountry,
-									Third:         tempResultThird.Third,
-									ThirdCountry:  tempResultThird.ThirdCountry,
-								}
-
-								if err := rows4.Scan(&tempResultFourth.Fourth, &tempResultFourth.FourthCountry); err != nil {
-									log.Fatal(err)
-								}
-
-								results = append(results, &tempResultFourth)
-							}
-
-						} else {
-							results = append(results, &tempResultThird)
-						}
-					}
-				} else {
-					results = append(results, &tempResultSecond)
-				}
-			}
-
-		}
-
-		//query := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[0], preferences[0])
-		//fmt.Println("query", query)
-		//rows, err := adapter.database.Connection.Query(ctx, query)
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
-		//
-		//defer rows.Close()
-		//
-		//for rows.Next() {
-		//	result := &Result{}
-		//	values := make([]interface{}, len(preferences))
-		//	pointers := make([]interface{}, len(preferences))
-		//
-		//	for i := range values {
-		//		pointers[i] = &values[i]
-		//	}
-		//
-		//	if err := rows.Scan(pointers...); err != nil {
-		//		log.Fatal(err)
-		//	}
-		//
-		//	for i, pref := range preferences {
-		//		switch pref {
-		//		case "mountain":
-		//			result.First = values[i].(string)
-		//		case "sea":
-		//			result.Second = values[i].(string)
-		//		case "excursion":
-		//			result.Third = values[i].(string)
-		//		case "health":
-		//			result.Fourth = values[i].(string)
-		//		}
-		//	}
-		//
-		//	result.FirstCountry = values[len(values)-1].(string)
-		//	results = append(results, result)
-		//}
-
-		return
-
-		//tour := strings.Split(UserPreferences, ",")
-		//fmt.Println("tour", tour)
-		//fmt.Println("len", len(tour))
-		//
-		//return
 	}
+	//preferences := strings.Split(UserPreferences, ",")
+	//query := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[0], preferences[0])
+	//rows1, err := adapter.database.Connection.Query(ctx, query)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//defer rows1.Close()
+	//
+	//for rows1.Next() {
+	//	result := &Result{}
+	//	if err := rows1.Scan(&result.First, &result.FirstCountry); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	querySecond := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[1], preferences[1])
+	//	rows2, err := adapter.database.Connection.Query(ctx, querySecond)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	defer rows2.Close()
+	//
+	//	for rows2.Next() {
+	//		tempResultSecond := Result{
+	//			First:        result.First,
+	//			FirstCountry: result.FirstCountry,
+	//		}
+	//		if err := rows2.Scan(&tempResultSecond.Second, &tempResultSecond.SecondCountry); err != nil {
+	//			log.Fatal(err)
+	//		}
+	//
+	//		if len(preferences) == 3 {
+	//			queryThird := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[2], preferences[2])
+	//			rows3, err := adapter.database.Connection.Query(ctx, queryThird)
+	//			if err != nil {
+	//				log.Fatal(err)
+	//			}
+	//
+	//			defer rows3.Close()
+	//
+	//			for rows3.Next() {
+	//				tempResultThird := Result{
+	//					First:         tempResultSecond.First,
+	//					FirstCountry:  tempResultSecond.FirstCountry,
+	//					Second:        tempResultSecond.Second,
+	//					SecondCountry: tempResultSecond.SecondCountry,
+	//				}
+	//				if err := rows3.Scan(&tempResultThird.Third, &tempResultThird.ThirdCountry); err != nil {
+	//					log.Fatal(err)
+	//				}
+	//
+	//				if len(preferences) == 4 {
+	//					queryFourth := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[3], preferences[3])
+	//					rows4, err := adapter.database.Connection.Query(ctx, queryFourth)
+	//					if err != nil {
+	//						log.Fatal(err)
+	//					}
+	//
+	//					defer rows4.Close()
+	//
+	//					for rows4.Next() {
+	//						fmt.Println("valll:", tempResultThird)
+	//						tempResultFourth := Result{
+	//							First:         tempResultThird.First,
+	//							FirstCountry:  tempResultThird.FirstCountry,
+	//							Second:        tempResultThird.Second,
+	//							SecondCountry: tempResultThird.SecondCountry,
+	//							Third:         tempResultThird.Third,
+	//							ThirdCountry:  tempResultThird.ThirdCountry,
+	//						}
+	//
+	//						if err := rows4.Scan(&tempResultFourth.Fourth, &tempResultFourth.FourthCountry); err != nil {
+	//							log.Fatal(err)
+	//						}
+	//
+	//						results = append(results, &tempResultFourth)
+	//					}
+	//
+	//				} else {
+	//					results = append(results, &tempResultThird)
+	//				}
+	//			}
+	//		} else {
+	//			results = append(results, &tempResultSecond)
+	//		}
+	//	}
+	//
+	//}
+	return
+	//query := fmt.Sprintf("SELECT %v, countries FROM projects.countries WHERE isNotNull(%v)", preferences[0], preferences[0])
+	//fmt.Println("query", query)
+	//rows, err := adapter.database.Connection.Query(ctx, query)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//defer rows.Close()
+	//
+	//for rows.Next() {
+	//	result := &Result{}
+	//	values := make([]interface{}, len(preferences))
+	//	pointers := make([]interface{}, len(preferences))
+	//
+	//	for i := range values {
+	//		pointers[i] = &values[i]
+	//	}
+	//
+	//	if err := rows.Scan(pointers...); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	for i, pref := range preferences {
+	//		switch pref {
+	//		case "mountain":
+	//			result.First = values[i].(string)
+	//		case "sea":
+	//			result.Second = values[i].(string)
+	//		case "excursion":
+	//			result.Third = values[i].(string)
+	//		case "health":
+	//			result.Fourth = values[i].(string)
+	//		}
+	//	}
+	//
+	//	result.FirstCountry = values[len(values)-1].(string)
+	//	results = append(results, result)
+	//}
+
+	//return
+
+	//tour := strings.Split(UserPreferences, ",")
+	//fmt.Println("tour", tour)
+	//fmt.Println("len", len(tour))
+	//
+	//return
 
 	//rows, err := adapter.database.Connection.Query(ctx, "SELECT * FROM projects.countries WHERE mountain != 'Нет'")
 	//if err != nil {
