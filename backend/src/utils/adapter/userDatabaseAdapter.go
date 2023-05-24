e58415d2d1a9f77d9f2122d3d5777be45a17991e
Misha Diagilev <mishadyagilev@gmail.com>
2023-05-24 14:43:02 +0300

    add fix 9 version

--- backend/src/utils/adapter/userDatabaseAdapter.go
+++ backend/src/utils/adapter/userDatabaseAdapter.go
@@ -21,15 +21,16 @@ type Tour struct {
 }
 
 type Output struct {
-	From        string    `json:"from"`
-	Preference  []string  `json:"preference"`
-	Filters     []string  `json:"filters"`
-	FiltersDone bool      `json:"filtersDone"`
-	FirstDone   bool      `json:"firstDone"`
-	SecondDone  bool      `json:"secondDone"`
-	ThirdDone   bool      `json:"thirdDone"`
-	FourthDone  bool      `json:"fourthDone"`
-	Result      []*Result `json:"result"`
+	From          string    `json:"from"`
+	Preference    []string  `json:"preference"`
+	Filters       []string  `json:"filters"`
+	FiltersDone   bool      `json:"filtersDone"`
+	FirstDone     bool      `json:"firstDone"`
+	SecondDone    bool      `json:"secondDone"`
+	ThirdDone     bool      `json:"thirdDone"`
+	FourthDone    bool      `json:"fourthDone"`
+	CountryFilter int       `json:"countryFilter"`
+	Result        []*Result `json:"result"`
 }
 
 type Result struct {
@@ -52,9 +53,64 @@ func CreateUserDatabaseAdapter(database *database.DBConnect) *UserDatabase {
 	return adapter
 }
 
-func (adapter *UserDatabase) getAllToursRecursive(ctx context.Context, preferences []string, addFilterVisa string, currentIndex int, currentResult *Result, results *[]*Result) error {
+func (adapter *UserDatabase) getAllToursRecursive(ctx context.Context, preferences []string, addFilterVisa string, countryFilter, currentIndex int, currentResult *Result, results *[]*Result) error {
 	if currentIndex == len(preferences) {
-		*results = append(*results, currentResult)
+		if countryFilter == 1 {
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry == "" {
+				if currentResult.SecondCountry == currentResult.FirstCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry != "" && currentResult.FourthCountry == "" {
+				if currentResult.SecondCountry == currentResult.FirstCountry &&
+					currentResult.SecondCountry == currentResult.ThirdCountry &&
+					currentResult.FirstCountry == currentResult.ThirdCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry != "" && currentResult.FourthCountry != "" {
+				if currentResult.SecondCountry == currentResult.FirstCountry &&
+					currentResult.SecondCountry == currentResult.ThirdCountry &&
+					currentResult.FirstCountry == currentResult.ThirdCountry &&
+					currentResult.FirstCountry == currentResult.FourthCountry &&
+					currentResult.SecondCountry == currentResult.FourthCountry &&
+					currentResult.ThirdCountry == currentResult.FourthCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+		} else if countryFilter == 2 {
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry == "" {
+				if currentResult.SecondCountry != currentResult.FirstCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry != "" && currentResult.FourthCountry == "" {
+				if currentResult.SecondCountry != currentResult.FirstCountry &&
+					currentResult.SecondCountry != currentResult.ThirdCountry &&
+					currentResult.FirstCountry != currentResult.ThirdCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+			if currentResult.SecondCountry != "" && currentResult.ThirdCountry != "" && currentResult.FourthCountry != "" {
+				if currentResult.SecondCountry != currentResult.FirstCountry &&
+					currentResult.SecondCountry != currentResult.ThirdCountry &&
+					currentResult.FirstCountry != currentResult.ThirdCountry &&
+					currentResult.FirstCountry != currentResult.FourthCountry &&
+					currentResult.SecondCountry != currentResult.FourthCountry &&
+					currentResult.ThirdCountry != currentResult.FourthCountry {
+					*results = append(*results, currentResult)
+				}
+			}
+
+		} else {
+			*results = append(*results, currentResult)
+		}
+
 		return nil
 	}
 
@@ -90,7 +146,7 @@ func (adapter *UserDatabase) getAllToursRecursive(ctx context.Context, preferenc
 			}
 		}
 
-		err := adapter.getAllToursRecursive(ctx, preferences, addFilterVisa, currentIndex+1, &result, results)
+		err := adapter.getAllToursRecursive(ctx, preferences, addFilterVisa, countryFilter, currentIndex+1, &result, results)
 		if err != nil {
 			return err
 		}
@@ -149,6 +205,7 @@ func (adapter *UserDatabase) GetAllTours(UserFromCountry, UserPreferences, UserF
 		return
 	} else {
 		addFilterVisa := ""
+		countryFilter := 0
 		arrayFilter := strings.Split(UserFilters, ",")
 		for i := range arrayFilter {
 			if arrayFilter[i] == "without_visa" {
@@ -163,11 +220,18 @@ func (adapter *UserDatabase) GetAllTours(UserFromCountry, UserPreferences, UserF
 				addFilterVisa = " AND visa == 'Электронная виза'"
 			}
 
+			if arrayFilter[i] == "in_one" {
+				countryFilter = 1
+			}
+
+			if arrayFilter[i] == "in_various" {
+				countryFilter = 2
+			}
 		}
 
 		preferences := strings.Split(UserPreferences, ",")
 		currentResult := &Result{}
-		err := adapter.getAllToursRecursive(ctx, preferences, addFilterVisa, 0, currentResult, &results)
+		err := adapter.getAllToursRecursive(ctx, preferences, addFilterVisa, countryFilter, 0, currentResult, &results)
 		if err != nil {
 			log.Fatal(err)
 		}
